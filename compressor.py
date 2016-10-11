3f93af061294957521c3b8644895869b3dac04dd
tonix0114 <tonix0114@gmail.com>
2016-10-11 21:49:54 +0900

    압축 방식 추가

---
+++ compressor.py
@@ -0,0 +1,11 @@
+import zlib
+
+ZLIB_COMPRESSION        = 1
+
+class ZlibCompressor:
+	def __init__(self):
+		self.supported = ZLIB_COMPRESSION
+		self.name="zlib"
+		
+	def uncompress(self, src):
+		return zlib.decompress(src)
